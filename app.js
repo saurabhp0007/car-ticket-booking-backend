f95fde03a02bafd1d66c1a80347b1aa540ba3da1
saurabh <Saurabh.Pandey@benchmarkestimating.com>
2025-04-12 16:27:07 +0530

    revert

--- app.js
+++ app.js
@@ -21,15 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use(cors({
-  origin: [
-    'https://www.subhamyaatravels.com',
-    'https://subhamyaatravels.com',
-    'https://car-reset-password-7bjmo96yi-saurabhs-projects-2660e0f6.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
-
 
 // Initialize Socket.IO with CORS options
 const io = socketIO(server, {
